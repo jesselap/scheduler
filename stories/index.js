ab9b44c1964fccdcb58e7729c409dfc25f0e8d60
jesselap <jesse.a.lapointe@gmail.com>
2021-06-08 14:54:19 -0400

    Refactor to generic onChange function name

--- stories/index.js
+++ stories/index.js
@@ -97,7 +97,7 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={event => action("setInterviewer")(interviewer.id)}
+      onChange={event => action("setInterviewer")(interviewer.id)}
     />
   ));
 
@@ -116,14 +116,14 @@ storiesOf("InterviewerList", module)
   .add("Initial", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("setInterviewer")}
     />
   ))
   .add("Preselected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
-      setInterviewer={action("setInterviewer")}
+      value={3}
+      onChange={action("setInterviewer")}
     />
   ));
   
