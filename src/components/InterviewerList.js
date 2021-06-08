cd61056f342b8f8e91da0ae705698f565510e3c5
jesselap <jesse.a.lapointe@gmail.com>
2021-06-08 19:57:13 -0400

    Remove comment

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,7 +3,6 @@ import InterviewerListItem from './InterviewerListItem';
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-  // const { interviewers, interviewer, setInterview } = props;
   const parsedInterviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
