25deca3b991d42d1f9ee9c118b3667bd17ab6153
jesselap <jesse.a.lapointe@gmail.com>
2021-06-14 19:34:46 -0400

    Form and Appointment component tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,13 +7,25 @@ import InterviewerList from "../InterviewerList"
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    setError("");
+    props.onSave(name, interviewer);
+  }
 
   const reset = () => {
     setName("");
     setInterviewer(null);
     props.onCancel();
   }
+
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -26,10 +38,9 @@ export default function Form(props) {
         placeholder="Enter Student Name"
         value={name}
         onChange={event => setName(event.target.value)}
-        /*
-          This must be a controlled component
-        */
+        data-testid="student-name-input"
       />
+      <section className="appointment__validation">{error}</section>
     </form>
     <InterviewerList 
       interviewers={props.interviewers} 
@@ -39,7 +50,7 @@ export default function Form(props) {
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={reset}>Cancel</Button>
-      <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+      <Button confirm onClick={validate}>Save</Button>
     </section>
   </section>
 </main>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import InterviewerListItem from './InterviewerListItem';
 import "components/InterviewerList.scss";
 
-export default function InterviewerList(props) {
+const InterviewerList = (props) => {
   const parsedInterviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
@@ -22,3 +23,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
